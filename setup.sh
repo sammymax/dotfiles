49fca45ba852ce8f925ace8ad8d4abf1f9742529
Philip Sun <philwsun@gmail.com>
2017-04-19 19:27:09 -0400

    Increase buffer size and install file manager

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ source ~/.bashrc
 
 ssh-keygen -t ed25519
 
-sudo apt-get install lightdm awesome rxvt-unicode-256color
+sudo apt-get install lightdm awesome rxvt-unicode-256color pcmanfm
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
