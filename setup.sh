58c4d52295b592981fe991b1ccf49223aae4fe5a
Philip Sun <philwsun@gmail.com>
2017-04-25 21:52:22 -0400

    Install 7zip, add options

--- setup.sh
+++ setup.sh
@@ -1,21 +1,44 @@
+#!/usr/bin/env bash
 sudo add-apt-repository ppa:git-core/ppa
-sudo add-apt-repository ppa:klaus-vormweg/awesome
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install vim htop openssh-server git tmux
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-source ~/.bashrc
+read -p "Install vim, htop, ssh, 7z, tmux, and git? [Y/n] " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo apt-get install vim htop openssh-server git tmux p7zip-full
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
-ssh-keygen -t ed25519
+read -p "Install pyenv and pyenv-virtualenv? [Y/n] " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+	source ~/.bashrc
+	git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+	source ~/.bashrc
+fi
 
-sudo apt-get install lightdm awesome rxvt-unicode-256color pcmanfm
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable
+read -p "Generate ssh key? [Y/n] " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	ssh-keygen -t ed25519
+fi
+
+read -p "Install UI? [Y/n] " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo add-apt-repository ppa:klaus-vormweg/awesome
+	sudo apt-get update
+	sudo apt-get install lightdm awesome rxvt-unicode-256color pcmanfm
+	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+	sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+	sudo apt-get update
+	sudo apt-get install google-chrome-stable
+fi
