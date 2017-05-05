b879781a81d04e9ae610424fb493c6692268023b
Philip Sun <philwsun@gmail.com>
2017-05-04 23:05:15 -0400

    Make pyenv work

--- setup.sh
+++ setup.sh
@@ -18,8 +18,12 @@ then
 	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 	echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+	echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 	source ~/.bashrc
 	git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+	sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+		libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+		xz-utils tk-dev
 	source ~/.bashrc
 fi
 
