abf214c9454ae56df3c6f61821c19ac38d91660a
Philip Sun <philwsun@gmail.com>
2017-05-30 00:03:56 -0400

    iptables config

--- setup.sh
+++ setup.sh
@@ -34,6 +34,19 @@ then
 	ssh-keygen -t ed25519
 fi
 
+read -p "Configure iptables? [Y/n] " -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+	sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+	sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+	sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+	sudo iptables -I INPUT 1 -i lo -j ACCEPT
+	sudo iptables -P INPUT DROP
+	sudo apt-get install iptables-persistent
+fi
+
 read -p "Install UI? [Y/n] " -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
