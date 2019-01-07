7f86f95af5b0b69640d1f6e9b7b3faaf81eaf4cf
Philip Sun <philwsun@gmail.com>
2019-01-07 13:23:06 -0500

    Show max CPU core temp on tint2 bar

---
+++ .config/tint2/tint2_cputemp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+maxtemp=0
+
+for temp in $(sensors | grep "^Core" | grep -e '+.*C' | cut -f 2 -d '+' | cut -f 1 -d ' ' | sed 's/°C//'); do
+	if [ $(echo "$temp > $maxtemp" | bc) -eq 1 ]; then
+		maxtemp=$temp
+	fi
+done
+
+echo $maxtemp | tr -d '[:space:]'
+echo "°C"
+#printf '%8s\n' "$maxtemp"
