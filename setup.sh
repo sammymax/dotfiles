88cd65966a63ce124046e9fa73408a3393973561
Philip Sun <philwsun@gmail.com>
2019-01-07 13:22:28 -0500

    Add iptables rule for mosh

--- setup.sh
+++ setup.sh
@@ -43,6 +43,7 @@ then
 	sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 	sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 	sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+	sudo iptables -I INPUT 1 -p udp --dport 60000:61000 -j ACCEPT
 	sudo iptables -I INPUT 1 -i lo -j ACCEPT
 	sudo iptables -P INPUT DROP
 	sudo apt-get install iptables-persistent
